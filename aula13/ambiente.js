4bbbcc8bdf956f4a1939124deb678815bf738dcd
dudasantts <duda.santts10@gmail.com>
2025-10-16 11:18:18 -0300

    Testes de repetições

---
+++ aula13/ambiente.js
@@ -0,0 +1,27 @@
+/*Teste lógico no final*/
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++ // c  c + 1
+} while (c <= 10) 
+    
+
+/*Teste lógico no início*/
+/*
+var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++ // c  c + 1
+}
+*/
+
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
